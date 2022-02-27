2be247f43bb2acddaae5ba9e9963fc79b36e5c1f
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2022-02-27 17:57:26 -0300

    Initial commit

---
+++ src/test/java/com/delaiglesia/plannerapi/PlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delaiglesia.plannerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
